3b93dbdfc5e9472c8f7bbbf882c282f077805604
Romain Pokrzywka <romain.pokrzywka@bluescape.com>
2016-03-21 10:31:53 -0700

    Add QQuickWindow::grabWindow() support to non-threaded render loop
    Task-number: QTBUG-52019
    Change-Id: I9604561efed37e522e0ba85fa9b27a63d3dd331b
    Reviewed-by: Andy Nichols <andy.nichols@theqtcompany.com>

--- src/plugins/scenegraph/softwarecontext/renderloop.cpp
+++ src/plugins/scenegraph/softwarecontext/renderloop.cpp
@@ -28,6 +28,7 @@
 #include <private/qquickwindow_p.h>
 #include <QElapsedTimer>
 #include <private/qquickprofiler_p.h>
+#include <qpa/qplatformbackingstore.h>
 
 RenderLoop::RenderLoop()
     : eventPending(false)
@@ -123,7 +124,7 @@ void RenderLoop::renderWindow(QQuickWindow *window)
     Q_QUICK_SG_PROFILE_RECORD(QQuickProfiler::SceneGraphRenderLoopFrame);
 
     if (data.grabOnly) {
-        // #### grabContent = qt_gl_read_framebuffer(window->size() * window->effectiveDevicePixelRatio(), false, false);
+        grabContent = static_cast<SoftwareContext::Renderer*>(cd->renderer)->backingStore()->handle()->toImage();
         data.grabOnly = false;
     }
 
